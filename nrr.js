4ff565a52553641ccf47d20d32be710a25b61472
Nick Rains <nnotreall@gmail.com>
2017-06-29 13:18:12 -0700

    Added details page and PDF

--- nrr.js
+++ nrr.js
@@ -25,6 +25,7 @@ d3.json('data.json', function(error, data) {
 
     drawTimeline();
     drawSkills();
+    drawDetails();
 });
 
 function calcBoxOffset(d) {
@@ -41,11 +42,19 @@ function calcBoxOffset(d) {
 
 function calcBoxSizes(d) {
     // returns class suffix, title y, subtitle y
-    if (d.type == 'certification') { return ['', 15, 28] };
+    if (d.type == 'certification') { 
+        return ['', 15, 28] 
+    };
     var h = nr.yScale(d.start) - nr.yScale(d.end);
-    if (d.displayWidth == 3 && h > 38) { return ['-lg', 20, 40] };
-    if (h < 34) { return ['-sm', 11, 20] };
-    if (d.title.length > 26 || d.subtitle.length > 30 ) { return ['-sm', 11, 20] };
+    if (d.displayWidth == 3 && h > 38) { 
+        return ['-lg', 20, 40] 
+    };
+    if (h < 34) { 
+        return ['-sm', 11, 20] 
+    };
+    if (d.title.length > 26 || d.subtitle.length > 30 ) { 
+        return ['-sm', 11, 20] 
+    };
     return ['', 15, 28];
 }
 
@@ -54,10 +63,11 @@ function drawTimeline() {
         minDate = d3.min(nr.timeline.map(d => d.start)),
         maxDate = d3.max(nr.timeline.map(d => d.end)),
         svg = d3.select('#timeline');
-
-    nr.yScale = d3.scaleTime()
-        .domain([maxDate, minDate])
-        .range([0, nr.height]);
+    d3.select('#timeline-div')
+        .insert('header', 'svg')
+        .attr('class', 'vizTitle')
+        .text('Timeline');
+    nr.yScale = d3.scaleTime().domain([maxDate, minDate]).range([0, nr.height]);
 
     // draw boxes
     var timeline = svg.selectAll('g')
@@ -68,7 +78,6 @@ function drawTimeline() {
             var y = d3.min([nr.yScale(d.end), nr.height - 25]) + 1;
             return 'translate(' + x + ',' + y + ')';
         });
-    
     timeline.append('rect')
         .attr('width', d => Math.floor(innerWidth / 3 * d.displayWidth) - 2)
         .attr('height', d => nr.yScale(d.start) - nr.yScale(d.end))
@@ -76,15 +85,12 @@ function drawTimeline() {
         .attr('class', d => d.type == 'employment' ? 'timeline-emp' : 'timeline-edu');
 
     // label boxes
-    var empText = timeline.append('text')
-        .attr('class', 'timeline-text');
-
+    var empText = timeline.append('text').attr('class', 'timeline-text');
     empText.append('tspan')
         .attr('class', d => 'timeline-title' + calcBoxSizes(d)[0])
         .attr('x', 5)
         .attr('y', d => calcBoxSizes(d)[1])
         .text(d => d.title);
-
     empText.append('tspan')
         .attr('class', d => 'timeline-subtitle' + calcBoxSizes(d)[0] )
         .attr('x', 5)
@@ -103,6 +109,9 @@ function drawTimeline() {
 function drawSkills() {
     var skills = Object.keys(nr.skills).map(function(x) { return { 'skill': x, 'level': nr.skills[x] } });
     var skillsDiv = d3.select('#skills');
+    skillsDiv.append('header')
+        .attr('class', 'vizTitle')
+        .text('Skills');
     var vizGap = 16;
     skillsDiv.selectAll('div')
         .data(skills).enter()
@@ -110,4 +119,47 @@ function drawSkills() {
         .text(d => d.skill)
         .attr('class', d => 'skill skill' + d.level)
         .style('width', d => (nr.width - vizGap) / 5 * d.level );
+}
+
+function formatDate(date) {
+    var month = String("00" + (1 + date.getMonth())).slice(-2);
+    return date.getFullYear() + '-' + month;
+}
+
+function drawDetails() {
+    var details = d3.select('#details');
+    
+    var empData = nr.timeline.filter(d => d.type == 'employment');
+    details.append('h2').text('Employment');
+    var empDivs = details.selectAll('div.empDetails')
+        .data(empData).enter()
+        .append('div')
+        .attr('class', 'experienceDetails empDetails');
+    empDivs.append('h4').html(function(d) {
+        return d.subtitle + ' <span class="subtle">at</span> ' + d.title + 
+        ' <span class="subtle">from</span> ' + formatDate(d.start)  + 
+        ' <span class="subtle">to</span> ' + formatDate(d.end)
+        });
+    empDivs.append('p').text(d => d.text);
+
+    var eduData = nr.timeline.filter(d => d.type == 'education'); 
+    details.append('h2').text('Education');
+    var eduDivs = details.selectAll('div.eduDetails')
+        .data(eduData).enter()
+        .append('div')
+        .attr('class', 'experienceDetails eduDetails');
+    eduDivs.append('h4').html(function(d) {
+        return '<span class="subtle">Studied</span> ' + d.subtitle + ' <span class="subtle">at</span> ' + 
+        d.title + ' <span class="subtle">from</span> ' + formatDate(d.start)  + 
+        ' <span class="subtle">to</span> ' + formatDate(d.end)
+        });
+
+    var certData = nr.timeline.filter(d => d.type == 'certification'); 
+    details.append('h2').text('Certification');
+    var certDivs = details.selectAll('div.certDetails')
+        .data(certData).enter()
+        .append('div')
+        .attr('class', 'experienceDetails certDetails');
+    certDivs.append('h4').html(d => d.title + ' on ' + formatDate(d.start));
+
 }
